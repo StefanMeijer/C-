6d9a0b23f60ec905bf909509396f69bcd7e4ec06
Stefan <stefan.meijer3@student.nhlstenden.com>
2022-04-25 19:44:11 +0200

    Week 1

---
+++ week1/Scenario1-1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scenario1_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            calcDiscount(101);
+        }
+
+        public static void calcDiscount(int quantity)
+        {
+            int discout = 0;
+
+            if (quantity < 10)
+            {
+                discout = 5;
+                
+            }
+            else if (quantity < 50)
+            {
+                discout = 10;
+            }
+            else if (quantity < 100)
+            {
+                discout = 15;
+            }
+            else
+            {
+                discout = 15;
+            }
+            Console.WriteLine(discout);
+            Console.Read();
+        }
+    }
+}
---
+++ week1/Scenario1-2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scenario1_2
+{
+    internal class Program
+    {
+        private static int fact = 1;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(libary(4));
+            Console.Read();
+        }
+
+       public static int libary(int n)
+       {
+            if (n > 1)
+            {
+                fact = fact * n;
+                return n = libary(n - 1);
+            } 
+            else
+            {
+                return fact;
+            }
+       }
+    }
+}
